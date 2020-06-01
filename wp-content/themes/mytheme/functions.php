fc03bfaa21685e29d6cfe4131f8e88ea0997d68b
umar-cedcoss <mohdumar@cedcoss.com>
2020-06-01 20:58:00 +0530

    theme post thumbnail addition

--- wp-content/themes/mytheme/functions.php
+++ wp-content/themes/mytheme/functions.php
@@ -28,6 +28,7 @@ function themeslug_enqueue_script() {
 add_action( 'wp_enqueue_scripts', 'themeslug_enqueue_style' );
 add_action( 'wp_enqueue_scripts', 'themeslug_enqueue_script' );
 
+
 /**
  * This is function description for register_dynamic_menu().
  *
@@ -45,3 +46,13 @@ function register_dynamic_menu() {
 	// Executing register with add_action hook.
 
 add_action( 'init', 'register_dynamic_menu' );
+if ( function_exists( 'add_theme_support' ) ) { 
+    add_theme_support( 'post-thumbnails' );
+    set_post_thumbnail_size( 150, 150, true ); // default Post Thumbnail dimensions (cropped)
+
+    // additional image sizes
+    // delete the next line if you do not need additional image sizes
+    add_image_size( 'category-thumb', 300, 9999 ); //300 pixels wide (and unlimited height)
+}
+
+
